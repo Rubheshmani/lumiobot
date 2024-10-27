c60b18c09ae42286f65c2c4bc1523c0495037d07
Rubheshmani <126094426+Rubheshmani@users.noreply.github.com>
2024-10-27 22:38:13 +0530

    Update alpaca_api.py

--- alpaca_api.py
+++ alpaca_api.py
@@ -1,12 +1,27 @@
 from alpaca_trade_api.rest import REST
 import config
 
+from alpaca_trade_api.rest import REST
+
 class AlpacaAPI:
-    def __init__(self):
-        self.api = REST(config.API_KEY, config.SECRET_KEY, config.BASE_URL, api_version='v2')
-        
+    def __init__(self, api_key, secret_key, base_url):  # type: ignore
+        self.api = REST(api_key, secret_key, base_url, api_version='v2')
+
     def fetch_historical_data(self, ticker, start, end, interval):
-        return self.api.get_barset(ticker, interval, start=start, end=end).df[ticker]
+        # Ensure the interval is supported for historical data
+        if interval == '1Day':
+            timeframe = '1Day'
+        else:
+            raise ValueError("Unsupported interval")
+
+        # Fetch historical data
+        try:
+            bars = self.api.get_bars(ticker, timeframe, start=start, end=end)
+            return bars.df[ticker]
+        except Exception as e:
+            print(f"Error fetching historical data: {e}")  # Log the error
+            return None  # Return None or handle it as needed
+
 
     def place_order(self, ticker, quantity, action):
         return self.api.submit_order(
